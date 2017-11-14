37fa18a799cd0eda3bbe8b6c305b5775b603d23d
Toni Panacek <toni.panacek@gmail.com>
2017-11-14 15:49:49 +0100

    functioning wiki page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/new'
+
+  # get 'articles/create'
+
+  # get 'articles/edit'
+
+  # get 'articles/update'
+
+  # get 'articles/delete'
+
+  resources :articles
+
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+Article.create(
+  title: Faker::HarryPotter.character,
+  content: Faker::HarryPotter.quote
+)
+end
